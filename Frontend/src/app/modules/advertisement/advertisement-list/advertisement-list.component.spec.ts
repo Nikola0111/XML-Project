fc843cf6499344fec930e58e5b4e63f1ac1b0c9a
abuhmiler <abuhmiler4@gmail.com>
2020-04-14 13:31:45 +0200

    Omogucen prikaz oglasa.

---
+++ Frontend/src/app/modules/advertisement/advertisement-list/advertisement-list.component.spec.ts
@@ -0,0 +1,34 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+
+import { AdvertisementListComponent } from './advertisement-list.component';
+
+describe('AdvertisementListComponent', () => {
+  let component: AdvertisementListComponent;
+  let fixture: ComponentFixture<AdvertisementListComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AdvertisementListComponent ],
+      imports: [
+        NoopAnimationsModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatTableModule,
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AdvertisementListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
