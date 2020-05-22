a17392ffc9bd875f9120f125b7abf56a5cddb24d
NemanjaKajzer <kajzapfk@gmail.com>
2020-05-22 14:18:53 +0200

    HTTPS radi

--- Backend/src/main/java/com/projekat/XML/XmlApplication.java
+++ Backend/src/main/java/com/projekat/XML/XmlApplication.java
@@ -9,8 +9,10 @@ import com.projekat.XML.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
 public class XmlApplication {
 
 	public static void main(String[] args) {
