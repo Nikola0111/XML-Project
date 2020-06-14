3053ac19a2494a209a63748fb8296ba0af8da94e
NemanjaKajzer <kajzapfk@gmail.com>
2020-06-14 10:10:16 +0200

    fix

--- Backend/src/main/java/com/projekat/XML/controller/AdvertisementController.java
+++ Backend/src/main/java/com/projekat/XML/controller/AdvertisementController.java
@@ -25,102 +25,93 @@ public class AdvertisementController {
 	@Autowired
 	private AdvertisementService advertisementService;
 
-	
-	@PreAuthorize("hasAuthority('advertisement:write')")	
+	@PreAuthorize("hasAuthority('advertisement:write')")
 	@RequestMapping(value = "/saveImage", method = RequestMethod.POST, headers = "content-type=multipart/form-data")
-    public ResponseEntity<Long> uploadImage(@RequestParam(value = "file", required = false) MultipartFile file) throws IOException {
+	public ResponseEntity<Long> uploadImage(@RequestParam(value = "file", required = false) MultipartFile file)
+			throws IOException {
 
-
-	System.out.println("Pogodio");
+		System.out.println("Pogodio");
 		System.out.println(file.getOriginalFilename());
 
 		advertisementService.saveImage(file);
 
-         return new ResponseEntity<>(HttpStatus.OK);
-}
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 
-@PreAuthorize("hasAuthority('advertisement:write')")	
-	@PostMapping(value="/save")
+	@PreAuthorize("hasAuthority('advertisement:write')")
+	@PostMapping(value = "/save")
 	public ResponseEntity<Long> save(@RequestBody AdvertisementCreationDTO advertisementDTO) {
 
-		System.out.println(advertisementDTO.getName()+advertisementDTO.getModel()+advertisementDTO.getBrand());
+		System.out.println(advertisementDTO.getName() + advertisementDTO.getModel() + advertisementDTO.getBrand());
 		System.out.println("AMIN");
-	
+
 		advertisementService.save(advertisementDTO);
 
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
-	
-
-<<<<<<< HEAD
-	@PreAuthorize("hasAnyRole('ROLE_ENDUSER', 'ROLE_AGENT')")	//privilegije, ne role
-=======
-	@PreAuthorize("hasAuthority('advertisement:read')")	
->>>>>>> 6ab06f0684a51d322937f406ce1458c8ed56bf62
-    @GetMapping(value = "/all")
-    public ResponseEntity<List<Advertisement>> getAll() {
-        List<Advertisement> advertisements = advertisementService.findAll();
-        return new ResponseEntity<>(advertisements, HttpStatus.OK);
+	@PreAuthorize("hasAuthority('advertisement:read')")
+	@GetMapping(value = "/all")
+	public ResponseEntity<List<Advertisement>> getAll() {
+		List<Advertisement> advertisements = advertisementService.findAll();
+		return new ResponseEntity<>(advertisements, HttpStatus.OK);
 	}
-	
 
-
-	@PostMapping(value="/filterAdv", consumes= MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(value = "/filterAdv", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<Advertisement>> filterAds(@RequestBody FilterAdsDTO filterAdsDTO) {
 		System.out.println("POGODIO");
 		return new ResponseEntity<>(advertisementService.filterAds(filterAdsDTO), HttpStatus.OK);
 	}
 
-	@PreAuthorize("hasAuthority('advertisement:read')")	
-	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE,  consumes= MediaType.APPLICATION_JSON_VALUE)
+	@PreAuthorize("hasAuthority('advertisement:read')")
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<AdvertisementDTO> getAdvertisement(@PathVariable Long id) {
 		Advertisement advertisement = advertisementService.findOneByid(id);
-		if(advertisement == null) {
+		if (advertisement == null) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 		return new ResponseEntity<>(new AdvertisementDTO(advertisement), HttpStatus.OK);
 	}
 
-	@PreAuthorize("hasAuthority('advertisement:write')")	
+	@PreAuthorize("hasAuthority('advertisement:write')")
 	@PostMapping(value = "/update", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Advertisement> updateAdvertisement(@RequestBody Advertisement advertisement) throws Exception{
-	    Advertisement ad = advertisementService.update(advertisement);
-	    System.out.println(ad.getDiscount());
-	    if(ad != null) {
-	        return new ResponseEntity<>(advertisementService.findOneByid(ad.getId()), HttpStatus.OK);
-        }
-	    else {
-	        return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
-        }
-    }
+	public ResponseEntity<Advertisement> updateAdvertisement(@RequestBody Advertisement advertisement)
+			throws Exception {
+		Advertisement ad = advertisementService.update(advertisement);
+		System.out.println(ad.getDiscount());
+		if (ad != null) {
+			return new ResponseEntity<>(advertisementService.findOneByid(ad.getId()), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+		}
+	}
 
-	@PreAuthorize("hasAuthority('comment:write')")	
+	@PreAuthorize("hasAuthority('comment:write')")
 	@PostMapping(value = "/saveCommentAndGrade", consumes = MediaType.APPLICATION_JSON_VALUE)
-	public  ResponseEntity<Long> saveCommentAndGrade(@RequestBody CommentDTO commentDTO){
+	public ResponseEntity<Long> saveCommentAndGrade(@RequestBody CommentDTO commentDTO) {
 		advertisementService.saveCommentAndGrade(commentDTO);
 
 		return new ResponseEntity<>((long) 1, HttpStatus.OK);
 
 	}
 
-	@PreAuthorize("hasAuthority('advertisement:read')")	
-	@GetMapping(value = "/preview/{id}", produces = MediaType.APPLICATION_JSON_VALUE,  consumes= MediaType.APPLICATION_JSON_VALUE)
+	@PreAuthorize("hasAuthority('advertisement:read')")
+	@GetMapping(value = "/preview/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<AdvertisementDTO> getAdvertisementPreview(@PathVariable Long id) {
 		AdvertisementDTO advertisementDTO = advertisementService.findAdAndComments(id);
-		if(advertisementDTO == null) {
+		if (advertisementDTO == null) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 		System.out.println(advertisementDTO.getGrade());
 		return new ResponseEntity<>(advertisementDTO, HttpStatus.OK);
 	}
 
-	@PreAuthorize("hasAuthority('comment:read')")	
-	@GetMapping(value = "/getAllComments/{id}", produces = MediaType.APPLICATION_JSON_VALUE,  consumes= MediaType.APPLICATION_JSON_VALUE)
+	@PreAuthorize("hasAuthority('comment:read')")
+	@GetMapping(value = "/getAllComments/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<CommentPreviewDTO>> getAllComments(@PathVariable Long id) {
 		System.out.println(id);
 		AdvertisementDTO advertisementDTO = advertisementService.findAdAndComments(id);
-		if(advertisementDTO == null) {
+		if (advertisementDTO == null) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 		System.out.println(advertisementDTO.getName());
@@ -128,35 +119,31 @@ public class AdvertisementController {
 		return new ResponseEntity<>(advertisementDTO.getComments(), HttpStatus.OK);
 	}
 
-
-	@PreAuthorize("hasAuthority('booking:read')")	
-	@GetMapping(value = "/getRentedCars/{id}", produces = MediaType.APPLICATION_JSON_VALUE,  consumes= MediaType.APPLICATION_JSON_VALUE)
+	@PreAuthorize("hasAuthority('booking:read')")
+	@GetMapping(value = "/getRentedCars/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<Long>> getRentedCars(@PathVariable Long id) {
 		List<Long> rentedCars = advertisementService.getRentedCars(id);
 		return new ResponseEntity<>(rentedCars, HttpStatus.OK);
 	}
 
-
-	@PreAuthorize("hasAuthority('advertisement:read')")	
-	@GetMapping(value = "/getAllByPostedBy/{id}", produces = MediaType.APPLICATION_JSON_VALUE,  consumes= MediaType.APPLICATION_JSON_VALUE)
+	@PreAuthorize("hasAuthority('advertisement:read')")
+	@GetMapping(value = "/getAllByPostedBy/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<Advertisement>> getAllByPostedBy(@PathVariable Long id) {
 		List<Advertisement> advertisements = advertisementService.getAllByPostedBy(id);
 		return new ResponseEntity<>(advertisements, HttpStatus.OK);
 	}
 
+	@PreAuthorize("hasAuthority('comment:read')")
+	@PostMapping(value = "/saveReply", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Long> saveReply(@RequestBody ReplyDTO replyDTO) {
 
-	@PreAuthorize("hasAuthority('comment:read')")	
-	@PostMapping(value = "/saveReply",  produces = MediaType.APPLICATION_JSON_VALUE,  consumes= MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Long> saveReply(@RequestBody ReplyDTO replyDTO){
-
-		//KAD RESIS U SERVISU SAMO ODKOMENTARISI
-		//advertisementService.saveReply(replyDTO);
-
+		// KAD RESIS U SERVISU SAMO ODKOMENTARISI
+		// advertisementService.saveReply(replyDTO);
 
 		return new ResponseEntity<>((long) 1, HttpStatus.OK);
 	}
 
-	@PreAuthorize("hasAuthority('advertisement:read')")	
+	@PreAuthorize("hasAuthority('advertisement:read')")
 	@GetMapping(value = "/getAllDetails", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<CarDetailsDTO>> getAllDetails() {
 		List<CarDetailsDTO> cardetails = advertisementService.getCarDetails();
@@ -164,7 +151,7 @@ public class AdvertisementController {
 		return new ResponseEntity<>(cardetails, HttpStatus.OK);
 	}
 
-	@PreAuthorize("hasAuthority('advertisement:write')")	
+	@PreAuthorize("hasAuthority('advertisement:write')")
 	@PostMapping(value = "/saveCarDetail", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Boolean> saveCarDetail(@RequestBody CarDetailsDTO carDetailsDTO) {
 		Boolean ret = advertisementService.saveCarDetail(carDetailsDTO);
@@ -172,7 +159,7 @@ public class AdvertisementController {
 		return new ResponseEntity<>(ret, HttpStatus.OK);
 	}
 
-	@PreAuthorize("hasAuthority('advertisement:write')")	
+	@PreAuthorize("hasAuthority('advertisement:write')")
 	@PostMapping(value = "/deleteCarDetail", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Boolean> deleteCarDetail(@RequestBody CarDetailsDTO carDetailsDTO) {
 		Boolean ret = advertisementService.deleteCarDetail(carDetailsDTO);
@@ -180,7 +167,7 @@ public class AdvertisementController {
 		return new ResponseEntity<>(ret, HttpStatus.OK);
 	}
 
-	@PreAuthorize("hasAuthority('advertisement:read')")	
+	@PreAuthorize("hasAuthority('advertisement:read')")
 	@GetMapping(value = "/getAllByUser", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<Advertisement>> getAllByUser() {
 
@@ -188,6 +175,5 @@ public class AdvertisementController {
 
 		return new ResponseEntity<>(ads, HttpStatus.OK);
 	}
-
 
 }
